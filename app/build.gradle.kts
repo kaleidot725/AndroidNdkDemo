56c698d696e8d54e64adc057144ecc7ebee38c24
kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2024-01-21 14:54:26 +0900

    Create native lib sample

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -47,10 +47,15 @@ android {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
+    externalNativeBuild {
+        cmake {
+            path("../nativeLib/src/main/cpp/CMakeLists.txt")
+        }
+    }
 }
 
 dependencies {
-
+    implementation(project(":nativelib"))
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
     implementation("androidx.activity:activity-compose:1.8.2")
--- app/src/main/java/jp/kaleidot725/ndkdemo/MainActivity.kt
+++ app/src/main/java/jp/kaleidot725/ndkdemo/MainActivity.kt
@@ -4,12 +4,16 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import jp.kaleidot725.nativelib.NativeLib
 import jp.kaleidot725.ndkdemo.ui.theme.AndroidNdkDemoTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,30 +21,17 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidNdkDemoTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    var text by remember { mutableStateOf("INIT") }
+                    val nativeLib by remember { mutableStateOf(NativeLib()) }
+                    Button(onClick = { text = nativeLib.stringFromJNI() }) {
+                        Text(text = text)
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidNdkDemoTheme {
-        Greeting("Android")
     }
 }
